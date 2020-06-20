33361e992948d77d54ed5097af0770c21bda42b6
luysla <dyana.tavares1@gmail.com>
2020-06-20 16:56:59 -0300

    Editei server

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express')
 
 const app = express()
-const port = process.env.PORT || 3000
-
-const baseURL = 'http://localhost:3000'
+const port = process.env.PORT
 
 app.use(express.json())
 
